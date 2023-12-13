13d2e00b65fd3e207f38725af97a30539ce4609c
Ramandeep Ramarayane <ramandeep.r49@gmail.com>
2023-12-14 01:09:54 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -7,10 +7,10 @@ const app = express();
 // set cors
 // app.use(cors());
 app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
 );
 
 app.use(express.json({ limit: "16kb" })); //limit the json size
@@ -19,7 +19,15 @@ app.use(express.static("public")); // to store public assets
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.send({ status: 200, data: "Welcome to Todo" });
+    res.status(200).json({
+        message: "ok",
+    });
 });
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes
+app.use("/api/v1/users", userRouter);
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ import { app } from "./app.js";
 const _PORT = process.env.PORT || 8000;
 
 dotenv.config({
-  path: "./.env",
+    path: "./.env",
 });
 
 connectDB()
-  .then(() => {
-    app.listen(_PORT, () => {
-      console.log(`---- Server is running at port ${_PORT} ----`);
+    .then(() => {
+        app.listen(_PORT, () => {
+            console.log(`---- ⚙️ Server is running at port ${_PORT} ----`);
+        });
+    })
+    .catch((err) => {
+        console.log(" !! MongoDB Connection Failed !!", err);
     });
-  })
-  .catch((err) => {
-    console.log(" !! MongoDB Connection Failed !!", err);
-  });
